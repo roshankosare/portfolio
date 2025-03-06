c2c48c1d5b1bad1ec71e436fa97497935b0faacc
Roshan Kosare <roshankosarepro@gmail.com>
2025-03-06 16:41:14 +0530

    project preview samll fix

--- src/components/projectPreview.tsx
+++ src/components/projectPreview.tsx
@@ -10,10 +10,13 @@ const ProjectPreview: React.FC<Props> = ({ image, name, onOpenDialog }) => {
   return (
     <div
       onClick={() => onOpenDialog()}
-      className="sm:w-[400px] sm:h-[300px]  bg-stone-950 rounded-4xl flex flex-col gap-y-4 py-5 px-5 cursor-pointer"
+      className=" max-w-[250px] w-full sm:max-w-[400px] h-auto   bg-stone-950  rounded-2xl sm:rounded-4xl flex flex-col  gap-y-2 sm:gap-y-4  py-2  px-2 sm:py-5 sm:px-5 cursor-pointer"
     >
-      <img src={image} className="w-full max-w-[400px] h-auto" />
-      <span className="sm:text-2xl text-xl font-bold">{name}</span>
+      <img
+        src={image}
+        className="w-full max-w-[250px] sm:max-w-[400px] h-auto"
+      />
+      <span className="sm:text-2xl text-md font-bold">{name}</span>
     </div>
   );
 };
